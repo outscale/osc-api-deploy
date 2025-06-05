973e8efdcaeec992643d085d29a9d7914fef6925
Lucas Rodriguez <lucas.rodriguez@outscale.com>
2025-06-05 10:03:52 +0000

    :bug: fix: Ignore schemas without properties

--- hacks/patch-nooneof.rb
+++ hacks/patch-nooneof.rb
@@ -13,15 +13,17 @@ require 'psych'
 api = Psych.load_file(ARGV[0])
 
 api["components"]["schemas"].each do |call_name, call|
-  call["properties"].each do |arg_name,arg_info|
-    if arg_info.key?("oneOf") then
-      arg_info["oneOf"][0].each do |key, val|
-        arg_info[key] = val
+  if call.key?("properties") then
+    call["properties"].each do |arg_name,arg_info|
+      if arg_info.key?("oneOf") then
+        arg_info["oneOf"][0].each do |key, val|
+          arg_info[key] = val
+        end
+        arg_info.delete("oneOf")
+      elsif arg_info.key?("items") and arg_info["items"].key?("oneOf") then
+        arg_info["items"] = arg_info["items"]["oneOf"][0]
+        arg_info["items"].delete("oneOf")
       end
-      arg_info.delete("oneOf")
-    elsif arg_info.key?("items") and arg_info["items"].key?("oneOf") then
-      arg_info["items"] = arg_info["items"]["oneOf"][0]
-      arg_info["items"].delete("oneOf")
     end
   end
 end
